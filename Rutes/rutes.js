41a853aadb5a93bf8814c01725f6d77a0cfe5129
Gerard Gascón <gerardgasconmoline8@gmail.com>
2021-12-11 12:47:27 +0100

    cool things

--- Rutes/rutes.js
+++ Rutes/rutes.js
@@ -8,36 +8,51 @@ request.open("GET", "/rutes.json", false);
 request.send(null);
 var json = JSON.parse(request.responseText);
 
-function loadRoute(index, variant){
+function loadRoute(index, variant) {
 	var innerHTML = "";
 	const element = json.rutes[index];
-	
+
 	document.title = `${element.nom} \u2013 Rutes BTT`;
 	header.innerHTML = `<h2>${element.nom}</h2>`;
-	if(element.variants.length > 1){
-		header.innerHTML += `<p style="font-size: 20px; margin-bottom: 0;">Variant ${variant + 1}</p>`;
+	if (element.variants.length > 1) {
+		header.innerHTML += `<p style="font-size: 20px; margin-bottom: 0;">Variant ${
+			variant + 1
+		}</p>`;
 		var variantButtons = "";
 		for (let i = 0; i < element.variants.length; i++) {
-			variantButtons += `<a class="variant-button" role="button" onclick="loadRoute(${index}, ${i})"><p>Variant ${i + 1}</p></a>`
+			variantButtons += `<a class="variant-button" role="button" onclick="loadRoute(${index}, ${i})"><p>Variant ${
+				i + 1
+			}</p></a>`;
 		}
 		variants.innerHTML = variantButtons;
-	}else{
+	} else {
 		variants.style.display = "none";
 	}
 
 	innerHTML += element.variants[variant].gpx_embed;
-	innerHTML += `<p><i class="far fa-calendar"></i> ${element.variants[variant].estació}</p>`;
+	innerHTML += "<div class='route-data'>"
+	innerHTML += `<p title="Estacions"><i class="far fa-calendar"></i> ${element.variants[variant].estació}</p>`;
+	innerHTML += `<p title="Circular"><i class="fas fa-undo-alt"></i> ${element.variants[variant].circular}</p>`;
+	//Average: 13km/h
+	innerHTML += `<p title="Duració (Només temps sobre rodes)"><i class="far fa-clock"></i> ${hoursToHHMM(element.variants[variant].temps)}</p>`;
+	innerHTML += "</div>"
 	innerHTML += element.variants[variant].descripció1;
-	innerHTML += "<div class='row'>"
+	innerHTML += "<div class='row'>";
 	for (let i = 0; i < element.galeria.length; i++) {
 		const image = element.galeria[i];
 		innerHTML += "<div class='column'>";
 		innerHTML += `<a href="${image}" data-lightbox="1"><img src="${image}" style="width:100%"></a>`;
-		innerHTML += "</div>"
+		innerHTML += "</div>";
 	}
 	innerHTML += "</div>";
 	innerHTML += element.variants[variant].descripció2;
 	card.innerHTML = innerHTML;
 
 	download.innerHTML = `<a href="${element.variants[variant].gpx}"><button class="link-button"><img src="/assets/gpx-file-format-variant.png"></button></a>`;
+}
+
+function hoursToHHMM(hours) {
+	var hh = Math.floor(Math.abs(hours));
+	var mm = Math.floor((Math.abs(hours) * 60) % 60);
+	return (hh < 10 ? "0" : "") + hh + "h " + (mm < 10 ? "0" : "") + mm + "'";
 }
--- index.js
+++ index.js
@@ -8,102 +8,113 @@ var json = JSON.parse(request.responseText);
 
 var listInnerHTML = "";
 for (let i = 0; i < json.rutes.length; i++) {
-    const element = json.rutes[i];
-    listInnerHTML += `<a href='${element.pagina}' style="text-decoration: none; color: inherit">`
-    listInnerHTML +=    `<div class='list_element' id='${element.dificultat.replace(' ', '')}' style="background-color: ${getColor(element)}; ${getColor(element)==="#F9A73E"?"color: #2f2c48;":""}">`;
-    listInnerHTML +=        `<h3 style='padding-top: 5px; padding-left: 5px;'>${element.nom}</h3>`;
-    listInnerHTML +=        "<div class='list_data'>";
-    listInnerHTML +=            `<p>Dificultat: ${element.dificultat}</p>`;
-    listInnerHTML +=            `<p>Desnivell positiu: ${element.desnivell}m</p>`;
-    listInnerHTML +=            `<p>Distància: ${element.distancia}km</p>`;
-    listInnerHTML +=        "</div>";
-    listInnerHTML +=    "</div>"
-    listInnerHTML += "</a>";
+	const element = json.rutes[i];
+	listInnerHTML += `<a href='${element.pagina}' style="text-decoration: none; color: inherit">`;
+	listInnerHTML += `<div class='list_element' id='${element.dificultat.replace(
+		" ",
+		""
+	)}' style="background-color: ${getColor(element)}; ${
+		getColor(element) === "#F9A73E" ? "color: #2f2c48;" : ""
+	}">`;
+	listInnerHTML += `<h3 style='padding-top: 5px; padding-left: 5px;'>${element.nom}</h3>`;
+	listInnerHTML += "<div class='list_data'>";
+	listInnerHTML += `<p>Dificultat: ${element.dificultat}</p>`;
+	listInnerHTML += `<p>Desnivell positiu: ${element.desnivell}m</p>`;
+	listInnerHTML += `<p>Distància: ${element.distancia}km</p>`;
+	listInnerHTML += "</div>";
+	listInnerHTML += "</div>";
+	listInnerHTML += "</a>";
 }
 list.innerHTML = listInnerHTML;
 
 var gridInnerHTML = "";
 for (let i = 0; i < json.rutes.length; i++) {
-    const element = json.rutes[i];
-    gridInnerHTML += `<div class='column' id='${element.dificultat.replace(' ', '')}'>`;
-    gridInnerHTML +=    `<a href='${element.pagina}'>`;
-    gridInnerHTML +=        `<img src="${element.imatge}" style="width:100%; border: 3px solid ${getColor(element)};">`;
-    gridInnerHTML +=    "</a>";
-    gridInnerHTML +=    `<div class="name" style="background: ${getColor(element)}; ${getColor(element)==="#F9A73E"?"color: #2f2c48;":""}">${element.nom}</div>`;
-    gridInnerHTML +=    `<div class="data">`;
-    gridInnerHTML +=        `<p>${element.dificultat}</p>`;
-    gridInnerHTML +=        `<p><i class="fas fa-angle-double-up"></i> ${element.desnivell}m</p>`;
-    gridInnerHTML +=        `<p><i class="fas fa-ruler"></i> ${element.distancia}km</p>`;
-    gridInnerHTML +=    `</div>`;
-    gridInnerHTML += "</div>";
+	const element = json.rutes[i];
+	gridInnerHTML += `<div class='column' id='${element.dificultat.replace(
+		" ",
+		""
+	)}'>`;
+	gridInnerHTML += `<a href='${element.pagina}'>`;
+	gridInnerHTML += `<img src="${
+		element.imatge
+	}" style="width:100%; border: 3px solid ${getColor(element)};">`;
+	gridInnerHTML += "</a>";
+	gridInnerHTML += `<div class="name" style="background: ${getColor(
+		element
+	)}; ${getColor(element) === "#F9A73E" ? "color: #2f2c48;" : ""}">${
+		element.nom
+	}</div>`;
+	gridInnerHTML += `<div class="data">`;
+	gridInnerHTML += `<p>${element.dificultat}</p>`;
+	gridInnerHTML += `<p><i class="fas fa-angle-double-up"></i> ${element.desnivell}m</p>`;
+	gridInnerHTML += `<p><i class="fas fa-ruler"></i> ${element.distancia}km</p>`;
+	gridInnerHTML += `</div>`;
+	gridInnerHTML += "</div>";
 }
 grid.innerHTML = gridInnerHTML;
 
-function getColor(json){
-    switch(json.dificultat){
-        case "Molt Difícil":
-            return "#BF212F";
-        case "Difícil":
-            return "#F9A73E";
-        case "Moderada":
-            return "#264B96";
-        case "Fàcil":
-            return "#0B8F49";
-    }
-    return "unset";
+function getColor(json) {
+	switch (json.dificultat) {
+		case "Molt Difícil":
+			return "#BF212F";
+		case "Difícil":
+			return "#F9A73E";
+		case "Moderada":
+			return "#264B96";
+		case "Fàcil":
+			return "#0B8F49";
+	}
+	return "unset";
 }
-function getColorByIndex(index){
-    switch(index){
-        case 3:
-            return "#BF212F";
-        case 2:
-            return "#F9A73E";
-        case 1:
-            return "#264B96";
-        case 0:
-            return "#0B8F49";
-    }
-    return "unset";
+function getColorByIndex(index) {
+	switch (index) {
+		case 3:
+			return "#BF212F";
+		case 2:
+			return "#F9A73E";
+		case 1:
+			return "#264B96";
+		case 0:
+			return "#0B8F49";
+	}
+	return "unset";
 }
-function getIndexName(index){
-    switch(index){
-        case 3:
-            return "MoltDifícil";
-        case 2:
-            return "Difícil";
-        case 1:
-            return "Moderada";
-        case 0:
-            return "Fàcil";
-    }
+function getIndexName(index) {
+	switch (index) {
+		case 3:
+			return "MoltDifícil";
+		case 2:
+			return "Difícil";
+		case 1:
+			return "Moderada";
+		case 0:
+			return "Fàcil";
+	}
 }
 
-var difficultiesSelected = [
-    true,
-    true,
-    true,
-    true
-];
+var difficultiesSelected = [true, true, true, true];
 
-function clickDifficulty(index, id){
-    difficultiesSelected[index] = !difficultiesSelected[index];
-    if(difficultiesSelected[index] === true){
-        document.querySelectorAll(`[id^='${id}']`)[0].style.background = getColorByIndex(index);
-        document.querySelectorAll(`[id^='${id}']`)[1].style.background = getColorByIndex(index);
+function clickDifficulty(index, id) {
+	difficultiesSelected[index] = !difficultiesSelected[index];
+	if (difficultiesSelected[index] === true) {
+		document.querySelectorAll(`[id^='${id}']`)[0].style.background =
+			getColorByIndex(index);
+		document.querySelectorAll(`[id^='${id}']`)[1].style.background =
+			getColorByIndex(index);
 
-        var routes = document.querySelectorAll(`#${getIndexName(index)}`);
-        for (let i = 0; i < routes.length; i++) {
-            const element = routes[i];
-            element.style.display = null;
-        }
-    }else{
-        document.querySelectorAll(`[id^='${id}']`)[0].style.background = "gray";
-        document.querySelectorAll(`[id^='${id}']`)[1].style.background = "gray";
+		var routes = document.querySelectorAll(`#${getIndexName(index)}`);
+		for (let i = 0; i < routes.length; i++) {
+			const element = routes[i];
+			element.style.display = null;
+		}
+	} else {
+		document.querySelectorAll(`[id^='${id}']`)[0].style.background = "gray";
+		document.querySelectorAll(`[id^='${id}']`)[1].style.background = "gray";
 
-        var routes = document.querySelectorAll(`#${getIndexName(index)}`);
-        for (let i = 0; i < routes.length; i++) {
-            const element = routes[i];
-            element.style.display = "none";
-        }
-    }
+		var routes = document.querySelectorAll(`#${getIndexName(index)}`);
+		for (let i = 0; i < routes.length; i++) {
+			const element = routes[i];
+			element.style.display = "none";
+		}
+	}
 }
